d4e1c635c74d5e8f2b4596404991acb55ff0a49e
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-10-07 11:58:51 +0200

    Bitalino created

---
+++ src/main/java/MultipleSclerosisPOJOs/Bitalino.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MultipleSclerosisPOJOs;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author noeli
+ */
+public class Bitalino implements Serializable{
+     private static final long serialVersionUID = 123456789L;
+     
+    
+}
