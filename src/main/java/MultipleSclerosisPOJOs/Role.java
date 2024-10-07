092b29a2b2ef8746edfbf937c0d760d33cfdbbea
nataliagarciasanchez <natalia.garciasanchez@usp.ceu.es>
2024-10-07 11:45:01 +0200

    Role Class

---
+++ src/main/java/MultipleSclerosisPOJOs/Role.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MultipleSclerosisPOJOs;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author nataliagarciasanchez
+ */
+public class Role {
+    private static final long serialVersionUID = -1008273412421410404L;
+
+    @Id 
+    @GeneratedValue(generator = "roles")
+    @TableGenerator(name = "roles", table = "sqlite_sequence", pkColumnName = "name", valueColumnName = "seq", pkColumnValue = "roles")
+    private Integer id;
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "role") 
+    private List<User> users;
+
+    public Role() {
+        super();
+    }
+
+    public Role(String name) {
+        super();
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, users);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        return Objects.equals(id, other.id) && Objects.equals(name, other.name) && Objects.equals(users, other.users);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
