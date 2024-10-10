2e4a2870cd6b006dc0eff65ccc771910e2b6c5d6
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-10-10 13:34:33 +0200

    Bitalino

--- src/main/java/MultipleSclerosisPOJOs/Bitalino.java
+++ src/main/java/MultipleSclerosisPOJOs/Bitalino.java
@@ -14,7 +14,46 @@ import java.sql.Date;
 public class Bitalino implements Serializable{
      private static final long serialVersionUID = 123456789L;
      private Date date;
+     private String signal_type;
+     private String file_path;
+     private Float duration;
+     private Integer clinical_historyID;
      
+     public Bitalino(){
+     super();
+     }
      
-    
+     public Bitalino (Date date,String signal_type,String file_path,Float duration){
+         this.date=date;
+         this.signal_type=signal_type;
+         this.file_path=file_path;
+         this.duration=duration;
+     }
+     
+     public Bitalino (Date date,String signal_type,String file_path,Float duration,Integer clinical_historyID){
+         this.date=date;
+         this.signal_type=signal_type;
+         this.file_path=file_path;
+         this.duration=duration;
+         this.clinical_historyID=clinical_historyID;
+     }
+     
+     public Date getDate() {
+	return date;
+    }
+     public void setDate(Date date) {
+	this.date = date;
+    }
+     public String getSignal_type(String signal_type) {
+	return signal_type;
+    } 
+     public void setSignal_type(String signal_type) {
+	this.signal_type = signal_type;
+    }
+     public String getFile_path(String file_path) {
+	return file_path;
+     }
+        }
 }
+
+     
