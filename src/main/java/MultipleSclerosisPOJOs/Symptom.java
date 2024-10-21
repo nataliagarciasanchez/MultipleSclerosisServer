f8db46468417da769daa7ef46bf8422002d1f245
andreamtnz <andrea.martinezpalacios@usp.ceu.es>
2024-10-21 12:31:38 +0200

    Symptom: attributes, getters, setters...

--- src/main/java/MultipleSclerosisPOJOs/Symptom.java
+++ src/main/java/MultipleSclerosisPOJOs/Symptom.java
@@ -4,10 +4,78 @@
  */
 package MultipleSclerosisPOJOs;
 
+import java.util.Objects;
+
 /**
  *
  * @author Andreoti
  */
 public class Symptom {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String name;
+    
+    public Symptom(){
+    super();
+    }
+
+    public Symptom(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Symptom{" + "id=" + id + ", name=" + name + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        hash = 61 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Symptom other = (Symptom) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+   
     
 }
