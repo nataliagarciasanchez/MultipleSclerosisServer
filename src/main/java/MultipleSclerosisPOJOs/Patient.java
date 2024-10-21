50cdc21dd97d46f973274798234356712f66e346
andreamtnz <andrea.martinezpalacios@usp.ceu.es>
2024-10-21 10:10:47 +0200

    Added getters, setters, equals, hashcode in Patient

--- src/main/java/MultipleSclerosisPOJOs/Patient.java
+++ src/main/java/MultipleSclerosisPOJOs/Patient.java
@@ -7,6 +7,7 @@ package MultipleSclerosisPOJOs;
 import java.io.Serializable;
 import java.sql.Date;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -23,6 +24,133 @@ public class Patient implements Serializable{
     private Integer phone;
     private Doctor doctor;
     private List<Report> reports;
+    
+    public Patient() {
+        super();
+    }
+
+    public Patient(Integer id, String name, Date dob, Gender gender, Integer phone, Doctor doctor, List<Report> reports) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+        this.phone = phone;
+        this.doctor = doctor;
+        this.reports = reports;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public List<Report> getReports() {
+        return reports;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void setReports(List<Report> reports) {
+        this.reports = reports;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" + "id=" + id + ", name=" + name + ", dob=" + dob + ", gender=" + gender + ", phone=" + phone + ", doctor=" + doctor + ", reports=" + reports + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + Objects.hashCode(this.dob);
+        hash = 53 * hash + Objects.hashCode(this.gender);
+        hash = 53 * hash + Objects.hashCode(this.phone);
+        hash = 53 * hash + Objects.hashCode(this.doctor);
+        hash = 53 * hash + Objects.hashCode(this.reports);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.dob, other.dob)) {
+            return false;
+        }
+        if (this.gender != other.gender) {
+            return false;
+        }
+        if (!Objects.equals(this.phone, other.phone)) {
+            return false;
+        }
+        if (!Objects.equals(this.doctor, other.doctor)) {
+            return false;
+        }
+        return Objects.equals(this.reports, other.reports);
+    }
+    
+    
 
     
     
