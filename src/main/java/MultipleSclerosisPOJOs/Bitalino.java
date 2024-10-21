6961b25b02805aaf1708ceebd0db5773d28e33f0
andreamtnz <andrea.martinezpalacios@usp.ceu.es>
2024-10-21 14:02:19 +0200

    Bitalino and Report - relationships added

--- src/main/java/MultipleSclerosisPOJOs/Bitalino.java
+++ src/main/java/MultipleSclerosisPOJOs/Bitalino.java
@@ -6,71 +6,152 @@ package MultipleSclerosisPOJOs;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.Objects;
+import javax.persistence.*;
 
 /**
  *
  * @author noeli
  */
+@Entity
+@Table (name= "bitalinos")
 public class Bitalino implements Serializable{
      private static final long serialVersionUID = 123456789L;
-     private Date date;
-     private String signal_type;
-     private String file_path;
-     private Float duration;
-     private Integer clinical_historyID;
      
-     public Bitalino(){
-     super();
-     }
+    @Id
+    @GeneratedValue (generator = "bitalinos")
+    @TableGenerator(name = "bitalinos", table = "sqlite_sequence",  pkColumnName = "name", valueColumnName = "seq", pkColumnValue = "bitalinos")
+    private Integer id;
+    private Date date;
+    private String signal_type;
+    private String file_path;
+    private Float duration;
+    
+    @ManyToOne
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
      
-     public Bitalino (Date date,String signal_type,String file_path,Float duration){
+    public Bitalino(){
+    super();
+    }
+     
+    public Bitalino (Date date,String signal_type,String file_path,Float duration){
          this.date=date;
          this.signal_type=signal_type;
          this.file_path=file_path;
          this.duration=duration;
      }
+
+    public Bitalino(Integer id, Date date, String signal_type, String file_path, Float duration, Report report) {
+        this.id = id;
+        this.date = date;
+        this.signal_type = signal_type;
+        this.file_path = file_path;
+        this.duration = duration;
+        this.report = report;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getSignal_type() {
+        return signal_type;
+    }
+
+    public String getFile_path() {
+        return file_path;
+    }
+
+    public Float getDuration() {
+        return duration;
+    }
+
+    public Report getReport() {
+        return report;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setSignal_type(String signal_type) {
+        this.signal_type = signal_type;
+    }
+
+    public void setFile_path(String file_path) {
+        this.file_path = file_path;
+    }
+
+    public void setDuration(Float duration) {
+        this.duration = duration;
+    }
+
+    public void setReport(Report report) {
+        this.report = report;
+    }
+
+    @Override
+    public String toString() {
+        return "Bitalino{" + "id=" + id + ", date=" + date + ", signal_type=" + signal_type + ", file_path=" + file_path + ", duration=" + duration + ", report=" + report + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.date);
+        hash = 79 * hash + Objects.hashCode(this.signal_type);
+        hash = 79 * hash + Objects.hashCode(this.file_path);
+        hash = 79 * hash + Objects.hashCode(this.duration);
+        hash = 79 * hash + Objects.hashCode(this.report);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Bitalino other = (Bitalino) obj;
+        if (!Objects.equals(this.signal_type, other.signal_type)) {
+            return false;
+        }
+        if (!Objects.equals(this.file_path, other.file_path)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.date, other.date)) {
+            return false;
+        }
+        if (!Objects.equals(this.duration, other.duration)) {
+            return false;
+        }
+        return Objects.equals(this.report, other.report);
+    }
      
-     public Bitalino (Date date,String signal_type,String file_path,Float duration,Integer clinical_historyID){
-         this.date=date;
-         this.signal_type=signal_type;
-         this.file_path=file_path;
-         this.duration=duration;
-         this.clinical_historyID=clinical_historyID;
-     }
+         
      
-     public Date getDate() {
-	return date;
-    }
-     public void setDate(Date date) {
-	this.date = date;
-    }
-     public String getSignal_type(String signal_type) {
-	return signal_type;
-    } 
-     public void setSignal_type(String signal_type) {
-	this.signal_type = signal_type;
-    }
-     public String getFile_path(String file_path) {
-	return file_path;
-     }
-     public void setFile_path(String file_path) {
-	this.file_path = file_path;
-     }
-     public Float getDuration(Float duration) {
-	return duration;
-    }
-      public void setDuration(Float duration) {
-	this.duration = duration;
-      }
-     public Integer getClinicalHistoryID(Integer clinicalHistoryID) {
-	return clinicalHistoryID;
-     }
-     public void SetClinicalHistoryID(Integer clinicalHistoryID) {
-	this.clinical_historyID= clinicalHistoryID;  
-     }
-       public String toString() {
-        return "Bitalino {" +"Date=" + date +" Signal Type=" + signal_type + " File path=" + file_path +  "Duration=" + duration + " Clinical History ID=" + clinical_historyID + '}';
-       }
         }
 
 
--- src/main/java/MultipleSclerosisPOJOs/Report.java
+++ src/main/java/MultipleSclerosisPOJOs/Report.java
@@ -7,17 +7,26 @@ package MultipleSclerosisPOJOs;
 import java.io.Serializable;
 import java.sql.Date;
 import java.util.List;
+import javax.persistence.*;
 
 /**
  *
  * @author laura
  */
+@Entity
+@Table (name= "reports")
 public class Report implements Serializable{
     
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue (generator = "reports")
+    @TableGenerator(name = "reports", table = "sqlite_sequence",  pkColumnName = "name", valueColumnName = "seq", pkColumnValue = "reports")
+    private Integer id;
     
     private Date date;
     private Patient patient;
+    
+    @OneToMany(mappedBy = "reports", cascade = CascadeType.ALL)
     private List<Bitalino> bitalinos;
     private List<Symptom> symptom;
 }
