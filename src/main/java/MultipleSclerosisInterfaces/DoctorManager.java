5c65c46cc6c3d1fd9fcd69bd9d18f42e44e44488
Maite Gomez del Rio <maite.gomezriovinuesa@usp.ceu.es>
2024-10-22 23:50:33 +0200

    Created interfaces

--- src/main/java/MultipleSclerosisInterfaces/DoctorManager.java
+++ src/main/java/MultipleSclerosisInterfaces/DoctorManager.java
@@ -4,10 +4,24 @@
  */
 package MultipleSclerosisInterfaces;
 
+import MultipleSclerosisPOJOs.Doctor;
+import MultipleSclerosisPOJOs.User;
+import java.util.List;
+
 /**
  *
  * @author nataliagarciasanchez
  */
-public class DoctorManager {
+public interface DoctorManager {
     
+    public void createDoctor(Doctor d);
+    public Doctor viewMyInfo(int id);
+    public List<Doctor> getListOfDoctors();
+    public void removeDoctorById(int id);
+    public void modifyDoctorInfo(int id, String name, String specialty, User user);
+    public void searchDoctorById(int id);
+    public List<Doctor> searchDoctorByName(String name);//es una lista porque puede haber doctores con el mismo nombre
+    public void assignPatient2Doctor(int doctorId, int patientId);
+    public void removePatientFromDoctor(int doctorId, int patientId);
+   
 }
---
+++ src/main/java/MultipleSclerosisInterfaces/PatientManager.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package MultipleSclerosisInterfaces;
+
+
+import MultipleSclerosisPOJOs.Patient;
+import MultipleSclerosisPOJOs.User;
+import java.util.List;
+
+/**
+ *
+ * @author maipa
+ */
+public interface PatientManager {
+    public void createPatient(Patient p);
+    public Patient viewMyInfo(int id);
+    public List<Patient> getListOfPatients();
+    public void removePatientById(int id);
+    public void modifyPatientInfo(int id, String name, String specialty, User user);
+    public void searchPatientById(int id);
+    public List<Patient> searchPatientByName(String name);
+    public List<Patient> searchPatientByDoctor(int doctorId);
+    public void assignSymptom2Patient(int symptomId, int patientId);
+    public void removePatientFromDoctor(int doctorId, int patientId);
+}
