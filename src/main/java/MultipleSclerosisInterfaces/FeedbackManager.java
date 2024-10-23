09b15143dba130bef5560d22e769c9b20e032636
andreamtnz <andrea.martinezpalacios@usp.ceu.es>
2024-10-23 18:16:20 +0200

    FeedbackManager

---
+++ src/main/java/MultipleSclerosisInterfaces/FeedbackManager.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package MultipleSclerosisInterfaces;
+
+import MultipleSclerosisPOJOs.Feedback;
+import java.sql.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Andreoti
+ */
+public interface FeedbackManager {
+    public void addFeedback(Feedback f);
+    public Feedback viewFeedbackInfo(Integer id);
+    public void removeFeedbackById(Integer id);
+    public List<Feedback> getListOfFeedbacksOfPatient(Integer patient_id);
+    public List<Feedback> getListOfFeedbacksOfDoctor(Integer doctor_id);
+    public Feedback searchFeedbackById(Integer id);
+    public void modifyFeedback(Integer id, String message, Date date, Integer doctor_id, Integer patient_id);
+    
+}
