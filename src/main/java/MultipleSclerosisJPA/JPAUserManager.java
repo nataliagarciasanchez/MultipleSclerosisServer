519b3042283e651aae7d341be08321102297c32b
Maite Gomez del Rio <maite.gomezriovinuesa@usp.ceu.es>
2024-10-23 13:33:34 +0200

    More of JPAUserManager

--- src/main/java/MultipleSclerosisJPA/JPAUserManager.java
+++ src/main/java/MultipleSclerosisJPA/JPAUserManager.java
@@ -7,9 +7,15 @@ package MultipleSclerosisJPA;
 import MultipleSclerosisInterfaces.UserManager;
 import MultipleSclerosisPOJOs.Role;
 import MultipleSclerosisPOJOs.User;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -76,27 +82,88 @@ public class JPAUserManager implements UserManager {
 
     @Override
     public List<Role> getRoles() {
-        
+        List<Role> roles = null;
+	    try {
+	    	Query query = em.createNativeQuery("SELECT * FROM roles", Role.class);
+	     roles = (List<Role>) query.getResultList();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return roles;
     }
 
     @Override
     public Role getRole(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Role role=null;
+		try {
+			Query query = em.createNativeQuery("SELECT * FROM Roles WHERE id ="+id, Role.class);
+			role = (Role) query.getSingleResult();
+		}catch(NoResultException nre) {
+			System.out.println("No role found with id: "+id);
+		}catch(Exception e) {
+			e.printStackTrace();
+	}
+		return role;
     }
 
     @Override
     public User getUser(String email) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        User user=null;
+	try {
+	Query query = em.createNativeQuery("SELECT * FROM Users WHERE email ="+email, User.class);
+	 user = (User) query.getSingleResult();
+	}catch(NoResultException nre) {
+		System.out.println("No user found with email: " + email);
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	return user;
     }
 
     @Override
     public User checkPassword(String email, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        User user=null;
+	try {
+            Query query = em.createNativeQuery("SELECT * from Users where email = ? and password= ?", User.class);
+			query.setParameter(1, email);
+			MessageDigest md = MessageDigest.getInstance("MD");
+			md.update(password.getBytes());
+			
+                        byte[] pw = md.digest();
+			query.setParameter(2, pw);
+			user = (User) query.getSingleResult();
+			
+	}catch(NoResultException e) {
+            System.out.println("No user found with the provided email and password.");
+
+	} catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(JPAUserManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	
+        return user;
+	
     }
 
     @Override
     public void changePassword(User user, String new_password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+
+            em.getTransaction().begin();
+	    Query query = em.createNativeQuery("UPDATE Users SET password = ? WHERE id = ?");
+	    MessageDigest md = MessageDigest.getInstance("MD");
+            md.update(new_password.getBytes());
+            byte[] new_Pw = md.digest();
+            query.setParameter(1, new_Pw);
+            query.setParameter(2, user.getId());
+	       
+	    query.executeUpdate();
+	    em.getTransaction().commit();
+            
+	} catch (NoSuchAlgorithmException e) {
+            if (em.getTransaction().isActive()) {
+	        em.getTransaction().rollback();
+	    }
+	}
     }
     
    
