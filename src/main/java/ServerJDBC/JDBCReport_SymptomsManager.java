09d43a9cf46950fd6215a50ea29e30ed832194fa
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-11-02 19:52:37 +0100

    JCBC REPORT-SYMPTOM 2/4

---
+++ src/main/java/ServerJDBC/JDBCReport_SymptomsManager.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ServerJDBC;
+
+import POJOs.Symptom;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author noeli
+ */
+public class JDBCReport_SymptomsManager {
+    
+    private JDBCManager manager;
+   
+    public JDBCReport_SymptomsManager(JDBCManager manager) {
+        this.manager = manager;
+        }
+    Report_Symptoms ("
+                + "      symptom_id INTEGER NOT NULL,"
+                + "      report_id INTEGER NOT NULL,"
+                + "      PRIMARY KEY(report_id, symptom_id)"
+                + "     FOREIGN KEY (patient_id) REFERENCES Patients(id)"
+                + "     FOREIGN KEY (report_id) REFERENCES Reports(id)"
+                + ");";
+     public void addSymptomToReport(Integer symptomId, Integer reportId){
+         try{
+            String sql = "INSERT INTO Report_Symptoms (symptom_id, report_id)"
+                    +"values (?,?)";
+            PreparedStatement p = manager.getConnection().prepareStatement(sql);
+            p.setInt(1,symptomId);
+            p.setInt(2,reportId);
+            p.executeUpdate();
+            p.close();
+            
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+     }
+    public void removeSymptomFromReport(Integer symptomId, Integer reportId){
+        try {
+            String sql = "DELETE FROM Report_Symptoms WHERE symptom_id, report_id";
+            PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+            prep.setInt(1, symptomId);
+            prep.setInt(1, reportId);
+
+            prep.executeUpdate();			
+	}catch(Exception e){
+	e.printStackTrace();
+	}
+    }
+    public void emptyReport(Integer reportId){
+   //delete all the symptoms associated with a report 
+    } 
+    public List <Symptom> getSymptomsFromReport(Integer reportId){
+        
+    }
+    
+}
