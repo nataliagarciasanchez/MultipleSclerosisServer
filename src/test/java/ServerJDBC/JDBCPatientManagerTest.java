aa36a0014fb360a2cdeb872a06110191c17dfd77
Noeliaauba <noelia.aubaarribas@usp.ceu.es>
2024-11-26 13:42:40 +0100

    tests from patient

--- src/test/java/ServerJDBC/JDBCPatientManagerTest.java
+++ src/test/java/ServerJDBC/JDBCPatientManagerTest.java
@@ -42,6 +42,7 @@ public class JDBCPatientManagerTest {
     public JDBCPatientManagerTest() {
     }
     
+    
     @BeforeAll
     public static void setUpClass() {
         jdbcManager = new JDBCManager();
@@ -84,7 +85,7 @@ public class JDBCPatientManagerTest {
         userManager.registerUser(u2);
         u3 = new User ("email", "password", r3);
         userManager.registerUser(u3);
-        d = new Doctor ("TempDoctor", "NEUROLOGY", u1);
+        d = new Doctor ("TempDoctor", "TempDoctorSurname", u1);
     }
     
     @AfterEach
@@ -103,8 +104,8 @@ public class JDBCPatientManagerTest {
         System.out.println("createPatient");
         java.sql.Date dob=Utilities.convertString2SqlDate("14/10/2003");
         Patient p = new Patient ("TempPatient","Auba","71045623A",dob, Gender.FEMALE,"678954326",d,u2);
-        System.out.println(p.toString());
         patientManager.registerPatient(p);
+        System.out.println(p.toString());
         Patient fetchedPatient = patientManager.getPatientById(p.getId());
         System.out.println(fetchedPatient.toString());
         assertNotNull(fetchedPatient);
@@ -119,8 +120,8 @@ public class JDBCPatientManagerTest {
         System.out.println("DeletePatient");
         java.sql.Date dob=Utilities.convertString2SqlDate("14/10/2003");
         Patient p = new Patient ("TempPatient","Auba","71045623A",dob, Gender.FEMALE,"678954326",d,u2);
-        System.out.println(p.toString());
         patientManager.registerPatient(p);
+        System.out.println(p.toString());
         List<Patient> PatientsBefore = patientManager.getListOfPatients();
         assertEquals(1, PatientsBefore.size());
         patientManager.removePatientById(p.getId());
@@ -137,6 +138,7 @@ public class JDBCPatientManagerTest {
         java.sql.Date dob=Utilities.convertString2SqlDate("14/10/2003");
         Patient p = new Patient ("TempPatient","Auba","71045623A",dob, Gender.FEMALE,"678954326",d,u2);
         patientManager.registerPatient(p);
+        System.out.println(p.toString());
         p.setName("UpdatedPatient");
         patientManager.updatePatient(p);
         Patient updatedPatient = patientManager.getPatientById(p.getId());
@@ -147,81 +149,139 @@ public class JDBCPatientManagerTest {
     /**
      * Test of getListOfPatients method, of class JDBCPatientManager.
      */
-   /* @Test
-    public void testGetListOfPatients() {
+    @Test
+    public void testGetListOfPatients() throws ParseException {
         System.out.println("getListOfPatients");
-        Doctor d1 = new Doctor ("Doctor1","NEUROLOGY",u1);
-        System.out.println(d1.toString());
-        doctorManager.createDoctor(d1);
-        Patient p1 = new Patient ("TempPatient1","Auba","71045623A",dob, Gender.FEMALE,d1);
-        Patient p2 = new Patient ("TempPatient2","Auba","71045623A",dob, Gender.MALE,d1);
-        patientManager.registerPatient(p1,u2);
-        patientManager.registerPatient(p2,u3);
+        java.sql.Date dob1=Utilities.convertString2SqlDate("17/8/2003");
+        java.sql.Date dob2=Utilities.convertString2SqlDate("14/10/2023");
+        Patient p1 = new Patient ("TempPatient1","Auba","71045623A",dob1, Gender.FEMALE,"678954326",d,u2);
+        Patient p2 = new Patient ("TempPatient2","Arribas","91145642F",dob2, Gender.MALE,"678934390",d,u3);
+        patientManager.registerPatient(p1);
+        patientManager.registerPatient(p2);
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
         List<Patient> patients = patientManager.getListOfPatients();
         assertEquals(2, patients.size());
-        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals("Patient1")));
-        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals("Patient2")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals("TempPatient1")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals("TempPatient2")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getSurname().equals("Auba")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getSurname().equals("Arribas")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getNIF().equals("71045623A")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getNIF().equals("91145642F")));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getDob().equals (dob1)));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getDob().equals (dob2)));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getGender().equals(Gender.FEMALE)));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getGender().equals(Gender.MALE)));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getPhone().equals(678954326)));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getPhone().equals(678934390)));
+        
         
     }
 
     /**
      * Test of getPatientById method, of class JDBCPatientManager.
      */
-   /* @Test
-    public void testGetPatientById() {
+   @Test
+    public void testGetPatientById() throws ParseException {
         System.out.println("getPatientById");
-        Patient p = new Patient("TempPatient");
-        User u= new User ("TempUser");
-        patientManager.registerPatient(p,u);
+        java.sql.Date dob=Utilities.convertString2SqlDate("14/10/2003");
+        Patient p = new Patient ("TempPatient","Auba","71045623A",dob, Gender.FEMALE,"678954326",d,u2);
+        patientManager.registerPatient(p);
+        System.out.println(p.toString());
         Patient fetchedPatient = patientManager.getPatientById(p.getId());
         assertNotNull(fetchedPatient);
         assertEquals(p.getId(), fetchedPatient.getId());
+        assertEquals(p.getName(), fetchedPatient.getName());
+        assertEquals(p.getSurname(), fetchedPatient.getSurname());
+        assertEquals(p.getNIF(), fetchedPatient.getNIF());
+        assertEquals(p.getDob(), fetchedPatient.getDob());
+        assertEquals(p.getGender(), fetchedPatient.getGender());
+        assertEquals(p.getPhone(), fetchedPatient.getPhone());
+        
     }
 
     /**
      * Test of getPatientByName method, of class JDBCPatientManager.
      */
-   /* @Test
-    public void testGetPatientByName() {
+    @Test
+    public void testGetPatientByName() throws ParseException {
         System.out.println("getPatientByName");
-        Patient p = new Patient("TempPatient");
-        User u= new User ("TempUser");
-        patientManager.registerPatient(p,u);
+        java.sql.Date dob=Utilities.convertString2SqlDate("14/10/2003");
+        Patient p = new Patient ("TempPatient","Auba","71045623A",dob, Gender.FEMALE,"678954326",d,u2);
+        patientManager.registerPatient(p);
+        System.out.println(p.toString());
         List<Patient> patients= patientManager.getPatientByName("TempPatient");
         assertNotNull(patients);
         assertFalse(patients.isEmpty());
-        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals("TempPatient")));
-        System.out.println("getPatientByName");
+        assertTrue(patients.stream().anyMatch(patient -> patient.getId().equals(p.getId())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals(p.getName())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getSurname().equals(p.getSurname())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getNIF().equals(p.getNIF())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getDob().equals(p.getDob())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getGender().equals(p.getGender())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getPhone().equals(p.getPhone())));
         
     }
 
     /**
      * Test of getPatientByUser method, of class JDBCPatientManager.
      */
-   /* @Test
-    public void  testGetPatientByUser(){
+   @Test
+    public void  testGetPatientByUser() throws ParseException{
         System.out.println("testGetPatientByUser");
-        Patient p = new Patient("TempPatient");
-        User u= new User ("TempUser");
-        patientManager.registerPatient(p,u);
-        Patient fetchedPatient= patientManager.getPatientByUser(u);
+        java.sql.Date dob=Utilities.convertString2SqlDate("14/10/2003");
+        Patient p = new Patient ("TempPatient","Auba","71045623A",dob, Gender.FEMALE,"678954326",d,u2);
+        patientManager.registerPatient(p);
+        System.out.println(p.toString());
+        Patient fetchedPatient = patientManager.getPatientByUser(u2);
         assertNotNull(fetchedPatient);
-        assertEquals(p.getUser(), fetchedPatient.getUser());
+        assertEquals(p.getId(), fetchedPatient.getId());
+        assertEquals(p.getName(), fetchedPatient.getName());
+        assertEquals(p.getSurname(), fetchedPatient.getSurname());
+        assertEquals(p.getNIF(), fetchedPatient.getNIF());
+        assertEquals(p.getDob(), fetchedPatient.getDob());
+        assertEquals(p.getGender(), fetchedPatient.getGender());
+        assertEquals(p.getPhone(), fetchedPatient.getPhone());
+        
         
     }
     
     /**
      * Test of getPatientsFromDoctor method, of class JDBCPatientManager.
      */
-    /*@Test
-    public void testGetPatientsFromDoctor(){
+    @Test
+    public void testGetPatientsFromDoctor() throws ParseException{
         System.out.println("testGetPatientsFromDoctor");
-        Doctor d= new Doctor ("TempDoctor");
-        Patient p = new Patient("TempPatient",d);
-        User u= new User ("TempUserPatient");
-        patientManager.registerPatient(p,u);
-        //Integer doctorId
+        java.sql.Date dob1=Utilities.convertString2SqlDate("24/04/2003");
+        Patient p = new Patient ("TempPatient","Auba","71045623A",dob1, Gender.FEMALE,"678954326",d,u2);
+        patientManager.registerPatient(p);
+        System.out.println(p.toString());
+        List<Patient> patients= patientManager.getPatientsFromDoctor(d.getId());
+        assertNotNull(patients);
+        assertFalse(patients.isEmpty());
+        assertTrue(patients.stream().anyMatch(patient -> patient.getId().equals(p.getId())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getName().equals(p.getName())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getSurname().equals(p.getSurname())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getNIF().equals(p.getNIF())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getDob().equals(p.getDob())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getGender().equals(p.getGender())));
+        assertTrue(patients.stream().anyMatch(patient -> patient.getPhone().equals(p.getPhone())));
+        
     }
-    */
+    
+    @Test
+    public void testGetDoctorFromPatient() throws ParseException{
+        System.out.println("testGetPatientsFromDoctor");
+        java.sql.Date dob1=Utilities.convertString2SqlDate("24/04/2003");
+        Patient p = new Patient ("TempPatient","Auba","71045623A",dob1, Gender.FEMALE,"678954326",d,u2);
+        patientManager.registerPatient(p);
+        System.out.println(p.toString());
+        int doctor_id= patientManager.getDoctorIdFromPatient(p);
+        assertNotNull(doctor_id);
+        assertEquals(p.getDoctor().getId(), doctor_id);
+        
+    }
+    
     
 }
+
